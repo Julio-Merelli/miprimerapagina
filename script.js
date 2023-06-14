5b4c4cdedb4df1908c7930b151746cc6838227b2
Julio-Merelli <merelli_julio@hotmail.com>
2023-06-14 00:03:08 -0300

    nuevas funcionalidades

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
 
+const toggleButton = document.getElementById("toggle-button");
+const sliderContainer = document.querySelector(".slider-container");
+const galleryContainer = document.querySelector(".gallery-container");
+const imgElements = document.querySelectorAll(".img_galeria");
+const prevButton = document.getElementById("anterior");
+const nextButton = document.getElementById("siguiente");
+const images = ["imagenes/dog1.png", "imagenes/dog2.png", "imagenes/dog3.png", "imagenes/dog4.png"];
+let currentImageIndex = 0;
+
+toggleButton.addEventListener("click", function() {
+  if (sliderContainer.style.display === "none") {
+    sliderContainer.style.display = "block";
+    galleryContainer.style.display = "none";
+    toggleButton.textContent = "Ver vista galería de imágenes";
+  } else {
+    sliderContainer.style.display = "none";
+    galleryContainer.style.display = "block";
+    toggleButton.textContent = "Ver vista deslizador de imágenes";
+  }
+});
+
+function showImage(index) {
+  imgElements.forEach(function(img) {
+    img.style.display = "none";
+  });
+  imgElements[index].style.display = "block";
+}
+
+prevButton.addEventListener("click", function() {
+  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+  showImage(currentImageIndex);
+});
+
+nextButton.addEventListener("click", function() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  showImage(currentImageIndex);
+});
+
+showImage(currentImageIndex);
+
+
+
+
+
+
+
+
 function showalert(){
     alert('sos curioso?????')
 }
@@ -7,18 +54,24 @@ function mostrarInicio(){
     document.getElementById("inicio").style.display = "block";
     document.getElementById("formulario").style.display = "none";
     document.getElementById("imagenes").style.display = "none";
+    
 }
 function mostrarFormulario(){
     document.getElementById("inicio").style.display = "none";
     document.getElementById("formulario").style.display = "block";
     document.getElementById("imagenes").style.display = "none";
+    
 }
 function mostrarImagenes(){
     document.getElementById("inicio").style.display = "none";
     document.getElementById("formulario").style.display = "none";
     document.getElementById("imagenes").style.display = "block";
+    
+
 
 }
+
+
 
 function mostrarmenuresp(){
   document.getElementById
@@ -72,8 +125,5 @@ function validateForm() {
 
 
 
-
-
- 
 
 
